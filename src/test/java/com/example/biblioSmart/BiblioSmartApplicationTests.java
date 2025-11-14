788ae1f0dfe5cab4d6f40189eaafc8938e0e6684
j1gnacio <j.burgos18@ufromail.cl>
2025-11-14 17:58:34 -0300

    cambio de nombre

--- src/test/java/com/example/biblioSmart/BiblioSmartApplicationTests.java
+++ src/test/java/com/example/biblioSmart/BiblioSmartApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BibloSmartApplicationTests {
+class BiblioSmartApplicationTests {
 
 	@Test
 	void contextLoads() {
